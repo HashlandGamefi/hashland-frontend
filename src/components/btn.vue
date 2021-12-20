575499c37736573c23f05225bfbae588ebd8e723
wyc <bemyself008@163.com>
2021-12-20 14:10:31 +0800

    Determine the authorization button optimization

--- src/components/btn.vue
+++ src/components/btn.vue
@@ -35,9 +35,9 @@ export default {
       this.$store.commit("setwalletstatus", true);
     },
     // 判断是否授权
-    isApproveFun(type, contractAdrdess) {
+    isApproveFun(type, contractAdrdess, isAddres = false) {//isAddres?true:false  true:传进来的是合约地址  false:传进来的是币种
       // console.log('当前币种%s是否授权: ', type);
-      if (type == 'hn') {
+      if (type === 'hn') {
         return new Promise(resolve => {
           hn().isApprovedForAll(this.getAccount, contractAdrdess).then(res => {
             // console.log('子组件方法--hn是否授权res: ', res);
@@ -51,7 +51,7 @@ export default {
             resolve(false)
           })
         })
-      } else if(type == 'hc') {
+      } else if(type === 'hc') {
         return new Promise(resolve => {
           hc().allowance(this.getAccount, contractAdrdess).then(res => {
             // console.log('子组件方法--hc是否授权res: ', res);
@@ -67,23 +67,34 @@ export default {
         })
       }else{
         return new Promise(resolve => {
-          erc20(token()[type]).allowance(this.getAccount,contractAdrdess).then(res => {
-            // console.log('子组件方法--busd是否授权res: ', res);
-            if (res.toString() > 0) {
-              resolve(true)
-            } else {
+          if(isAddres){
+            erc20(type).allowance(this.getAccount,contractAdrdess).then(res => {
+              if (res.toString() > 0) {
+                resolve(true)
+              } else {
+                resolve(false)
+              }
+            }).catch(err => {
               resolve(false)
-            }
-          }).catch(err => {
-            console.log('子组件方法--hc是否授权err: ', err);
-            resolve(false)
-          })
+            })
+          }else{
+            erc20(token()[type]).allowance(this.getAccount,contractAdrdess).then(res => {
+              if (res.toString() > 0) {
+                resolve(true)
+              } else {
+                resolve(false)
+              }
+            }).catch(err => {
+              console.log('子组件方法--hc是否授权err: ', err);
+              resolve(false)
+            })
+          }
         })
       }
     },
     // 去授权
-    goApproveFun(type, contractAdrdess) {
-      if (type == 'hn') {
+    goApproveFun(type, contractAdrdess, isAddres = false) {//isAddres?true:false  true:传进来的是合约地址  false:传进来的是币种
+      if (type === 'hn') {
         return new Promise((resolve,reject) => {
           hn().connect(getSigner()).setApprovalForAll(contractAdrdess, true).then(async res => {
             console.log('子组件方法--hn去授权res: ', res);
@@ -98,7 +109,7 @@ export default {
             reject(false)
           })
         })
-      }else if(type == 'hc') {
+      }else if(type === 'hc') {
         const TOKEN_amount = "50000000000000000000000000000000000000000000000000000000000";
         return new Promise((resolve,reject) => {
           hc().connect(getSigner()).approve(contractAdrdess, TOKEN_amount).then(async res => {
@@ -117,18 +128,33 @@ export default {
       }else{
         const TOKEN_amount = '50000000000000000000000000000000000000000000000000000000000';
         return new Promise((resolve,reject) => {
-          erc20(token()[type]).connect(getSigner()).approve(contractAdrdess,TOKEN_amount).then(async res => {
-            console.log('子组件方法--busd去授权res: ', res);
-            const etReceipt = await res.wait();
-            if(etReceipt.status == 1){
-              resolve(true)
-            }else{
-              resolve(false)
-            }
-          }).catch(err => {
-            console.log('子组件方法--busd去授权err: ', err);
-            reject(false)
-          })
+          if(isAddres){
+            erc20(type).connect(getSigner()).approve(contractAdrdess,TOKEN_amount).then(async res => {
+              // console.log('子组件方法--busd去授权res: ', res);
+              const etReceipt = await res.wait();
+              if(etReceipt.status == 1){
+                resolve(true)
+              }else{
+                resolve(false)
+              }
+            }).catch(err => {
+              console.log('子组件方法--busd去授权err: ', err);
+              reject(false)
+            })
+          }else{
+            erc20(token()[type]).connect(getSigner()).approve(contractAdrdess,TOKEN_amount).then(async res => {
+              // console.log('子组件方法--busd去授权res: ', res);
+              const etReceipt = await res.wait();
+              if(etReceipt.status == 1){
+                resolve(true)
+              }else{
+                resolve(false)
+              }
+            }).catch(err => {
+              console.log('子组件方法--busd去授权err: ', err);
+              reject(false)
+            })
+          }
         })
       }
     },
