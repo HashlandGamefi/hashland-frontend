bbf3fc6ef70a6cf4efc0523763e88d1d9950146f
wyc <bemyself008@163.com>
2021-12-23 17:09:42 +0800

    First edition of the invitation page

--- src/views/Invite/index.vue
+++ src/views/Invite/index.vue
@@ -24,18 +24,19 @@
           <img :src="`${$store.state.imgUrl}inviteimg1.png`" class="center_img" />
           <div class="positionbox">
             <div class="top_box">
-              <span class="span1 fontsize32">1</span>
-              <span class="span2 fontsize12">Ranking</span>
+              <span class="span1 fontsize22" v-if="!isNumber">{{$t(ishowRankNum)}}</span>
+              <span class="span1 fontsize22" v-else>{{ishowRankNum}}</span>
+              <span class="span2 fontsize12">{{ $t("message.invite.txt15") }}</span>
             </div>
             <div class="bottom_box">
               <div class="one_line">
-                <span class="span1 fontsize32">11.5</span>
-                <span class="span2 fontsize12">战力</span>
+                <span class="span1 fontsize22">{{CENUM}}</span>
+                <span class="span2 fontsize12">{{ $t("message.invite.txt16") }}</span>
               </div>
               <div class="one_line">
-                <span class="span1 fontsize32">100</span>
+                <span class="span1 fontsize22">{{CENUM_ratio}}%</span>
                 <!-- <span class="span2 fontsize12">Territory proportion</span> -->
-                <span class="span2 fontsize12">占比</span>
+                <span class="span2 fontsize12">{{ $t("message.invite.txt17") }}</span>
               </div>
             </div>
           </div>
@@ -49,11 +50,11 @@
           </div>
           <div class="boxs_reward">
             <div class="onebox">
-              <span class="span1 fontsize16">累积奖励</span>
+              <span class="span1 fontsize16">{{ $t("message.invite.txt18") }}</span>
               <span class="span1 fontsize16">{{add_rewards}}</span>
             </div>
             <div class="onebox">
-              <span class="span1 fontsize16">可提取奖励</span>
+              <span class="span1 fontsize16">{{ $t("message.invite.txt19") }}</span>
               <div class="btn_box">
                 <span class="span1 fontsize16"  v-if="rewards == 0">{{rewards}}</span>
                 <span class="span1 fontsize16" v-else>
@@ -64,25 +65,25 @@
                     :duration="1500"
                   ></countTo>
                 </span>
-                <span class="btn" @click="extractClick">提取<BtnLoading :isloading="extract_btnloading"></BtnLoading></span>
+                <span class="btn fontsize16" @click="extractClick">{{ $t("message.invite.txt20") }}<BtnLoading :isloading="extract_btnloading"></BtnLoading></span>
               </div>
             </div>
           </div>
         </div>
       </div>
-      <span class="Ranking fontsize22">战队排行</span>
-      <span class="Ranking_txt fontsize12_400">The future DAO representatives</span>
+      <span class="Ranking fontsize22">{{ $t("message.invite.txt21") }}</span>
+      <span class="Ranking_txt fontsize12_400">{{ $t("message.invite.txt22") }}</span>
       <div class="listbox">
         <div class="topline">
-          <div class="title_onebox fontsize18">Ranking</div>
+          <div class="title_onebox fontsize18">{{ $t("message.invite.txt15") }}</div>
           <div class="onebox">
-            <div class="insertbox1 fontsize18">战力</div>
+            <div class="insertbox1 fontsize18">{{ $t("message.invite.txt23") }}</div>
           </div>
           <div class="onebox">
-            <div class="insertbox1 fontsize18">Address</div>
+            <div class="insertbox1 fontsize18">{{ $t("message.invite.txt24") }}</div>
           </div>
           <div class="onebox">
-            <div class="insertbox2 fontsize18">Territory proportion</div>
+            <div class="insertbox2 fontsize18">{{ $t("message.invite.txt25") }}</div>
           </div>
         </div>
         <div class="bottomline">
@@ -100,16 +101,19 @@
               <span class="pad_left fontsize16" v-else>{{index + 1}}</span>
             </div>
             <div class="onebox">
-              <div class="insertbox1 fontsize16">{{item.hcnum}}</div>
+              <div class="insertbox1 fontsize16">{{item.stakedHC}}</div>
             </div>
             <div class="onebox">
               <div class="insertbox1 fontsize16">{{item.address}}</div>
             </div>
             <div class="onebox">
-              <div class="insertbox2 fontsize16">{{item.btcnum}}</div>
+              <div class="insertbox2 fontsize16">{{item.ratio}}%</div>
             </div>
           </div>
-          <NoData v-if="list.length == 0" :isshow="false"></NoData>
+          <div class="loadingbox fontsize16" v-if="list.length == 0 && pageshowLoading" >
+            Loading...
+          </div>
+          <NoData v-else-if="list.length == 0 && !pageshowLoading" :isshow="false"></NoData>
         </div>
       </div>
     </div>
@@ -129,6 +133,11 @@ import { invitePool,getSigner,invitePoolInfo } from "hashland-sdk";
 export default {
   data () {
     return {
+      pageshowLoading: true,// 数据没有加载完之前显示loading
+      isNumber:false,
+      ishowRankNum:'message.invite.txt27',//我的战队排名
+      CENUM:0,//战力
+      CENUM_ratio:0,//战力比
       btcStarValue:0,// btc 可提取初始值
       timernull:null,//定时器对象
       btcnumShow:'12345678', // 数字滚动插件默认显示小数位数8位
@@ -139,23 +148,16 @@ export default {
       btntxt: "", // 弹窗页面的确认按钮
       word: "", //弹窗提示文字
       proupDis: false, // 弹窗展示消失变量
-      list:[
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10},
-        {address:'0X020X020X02…0X020X020X02',btcnum:123,hcnum:10}
-      ],
+      list:[],
       peopleAddress: "",
       redonlyDis: false, //input输入框  是否只读
       dis: false, // 确认按钮 是否禁用
       userlist:[
-        '0x16FEC748C51B702eCC4ACCe122EcF9059f7fBd1C'
+        '0x16FEC748C51B702eCC4ACCe122EcF9059f7fBd1C',
+        '0x1b8651a2D6Bd1BA4eE3E053930aeaE612f2489D3',
+        '0xA30D18C731c9944F904fFB1011c17B75280d2A08',
+        '0xf379d24dCE0Bb73d87d3499D4F1cC87F0Bd0091F',
+        '0xEeF038C88884fFb8A22Afd516c91690d1666ED18',
       ]
     }
   },
@@ -224,7 +226,12 @@ export default {
         return
       }
       if(this.peopleAddress.toLocaleLowerCase() == this.getAccount.toLocaleLowerCase()){
-        this.$common.selectLang("不能绑定自己", "NO", this)
+        this.$common.selectLang("不能绑定自己", "Cannot bind yourself", this)
+        this.peopleAddress = ''
+        return
+      }
+      if(this.userlist.indexOf(this.peopleAddress.toLocaleLowerCase()) == -1){
+        this.$common.selectLang("该战队暂未开放，请选择其他战队", "This team is not yet open, please choose another team", this)
         this.peopleAddress = ''
         return
       }
@@ -285,17 +292,146 @@ export default {
     },
     // 获取列表
     getUserList(){
-      // invitePoolInfo.getInviterInfo()
-      invitePool().getInviterUsersBySize(this.getAccount,0,21).then(res => {
-        console.log('获取某领主的基于指针(从0开始)和数量的被邀请人的地址数组和最后一个数据的指针: ',res);
-        res[0].map(item => {
-          return {
-            item:this.$common.getSubStr(item,5)
+      this.getDataBaseInfo().then(res => {
+        console.log('封装返回的res: ', res);
+        if(res.arr.length == 0){
+          this.list = []
+          this.pageshowLoading = false
+        }else{
+          console.log("进入else",res)
+          this.list = res.arr.filter(item => {
+            return item.status
+          })
+          for (let index = 0; index < this.list.length; index++) {
+            const ele = this.list[index];
+            if(this.getAccount.toLocaleLowerCase() == ele.inviter.toLocaleLowerCase()){
+              console.log("返回数据多会放")
+              let RankNum = this.list.findIndex(item => this.getAccount.toLocaleLowerCase() == item.inviter.toLocaleLowerCase() );
+              this.ishowRankNum = RankNum + 1
+              this.isNumber = true
+              this.CENUM = this.$common.getBit(ele.stakedHC, 2) //战力
+              this.CENUM_ratio = this.$common.getBit((ele.stakedHC / res.data), 2)//战力比
+              return
+            }else{
+              console.log("else大明风华")
+              this.ishowRankNum = 'message.invite.txt27'
+              this.isNumber = false
+            }
+          }
+        }
+      })
+      return
+      invitePoolInfo.getInviterInfo(10,0,'stakedHC','desc',this.getAccount).then(response => {
+        console.log('response: ', response);
+
+        // console.log('获取某领主的基于指针(从0开始)和数量的被邀请人的地址数组和最后一个数据的指针: ',res);
+        let resres = JSON.parse(JSON.stringify(response))
+        // let res = JSON.parse(JSON.stringify(response))
+        let res = {
+          data:{
+            inviterInfos:[{
+              ratio:20.22,
+              address:'0x16FEC748C51B702eCC4ACCe122EcF9059f7fBd1C',
+              stakedHC:1200
+            },
+            {
+              ratio:12.12,
+              address:'0xEeF038C88884fFb8A22Afd516c91690d1666ED18',
+              stakedHC:20000
+            },
+            {
+              ratio:50.2,
+              address:'0x16FEC748C51B702eCC4ACCe122EcF9059f7fBdvv',
+              stakedHC:400
+            }
+            ]
+          }
+        }
+        invitePool().stake().then(data => {
+          console.log('总战力data: ', data);
+          res.data.inviterInfos.forEach((element,i) => {
+            if(this.userlist.indexOf(element.address.toLocaleLowerCase()) != -1){
+              element.ratio = this.$common.getBit((element.stakedHC / data.toNumber()), 2)
+              console.log('element.stakedHC / data.toNumber(): ', element.stakedHC / data.toNumber())
+              element.address = this.$common.getSubStr(element.address,5)
+              element.stakedHC = this.$common.getBit(element.stakedHC, 2)
+            }else{
+              res.data.inviterInfos.splice(i,1)
+            }
+          });
+          this.list = res.data.inviterInfos
+          console.log('res.data.inviterInfos: ', res.data.inviterInfos);
+
+          for (let index = 0; index < resres.data.inviterInfos.length; index++) {
+            const ele = resres.data.inviterInfos[index];
+            if(this.getAccount.toLocaleLowerCase() == ele.address.toLocaleLowerCase()){
+              let RankNum = resres.data.inviterInfos.findIndex(item => this.getAccount.toLocaleLowerCase() == item.address.toLocaleLowerCase() );
+              this.ishowRankNum = RankNum + 1
+              this.isNumber = true
+              this.CENUM = this.$common.getBit(ele.stakedHC, 2) //战力
+              this.CENUM_ratio = this.$common.getBit((ele.stakedHC / data.toNumber()), 2)//战力比
+            }else{
+              this.ishowRankNum = 'message.invite.txt27'
+              this.isNumber = false
+            }
           }
         })
-        // this.list = res[0]
-      }).catch(err => {
-        console.log('获取某领主的基于指针err: ', err);
+      })
+    },
+    getDataBaseInfo(){
+      let ConversionArr = this.userlist.map(item => {
+        return item.toLocaleLowerCase()
+      })
+      return new Promise((resolve,reject) => {
+        invitePoolInfo.getInviterInfo(10,0,'stakedHC','desc',this.getAccount).then(res => {
+          invitePool().stake().then(data => {
+            if(res.data.inviterInfos.length == 0){
+              let res = [{
+                    ratio:20.22,
+                    inviter:'0x16FEC748C51B702eCC4ACCe122EcF9059f7fBd1C',
+                    stakedHC:1200
+                  },
+                  {
+                    ratio:12.12,
+                    inviter:'0xEeF038C88884fFb8A22Afd516c91690d1666ED18',
+                    stakedHC:20000
+                  },
+                  {
+                    ratio:50.2,
+                    inviter:'0x16FEC748C51B702eCC4ACCe122EcF9059f7fBdvv',
+                    stakedHC:400
+                  }
+                  ]
+              res.forEach(element => {
+                if(ConversionArr.indexOf(element.inviter.toLocaleLowerCase()) != -1){
+                  element.status = true
+                }else{
+                  element.status = false
+                }
+                element.ratio = this.$common.getBit((element.stakedHC / data.toNumber()), 2)
+                element.address = this.$common.getSubStr(element.inviter,5)
+                element.stakedHC = this.$common.getBit(element.stakedHC, 2)
+              })
+              resolve({'arr': res, 'msg':'Success','data':data.toNumber()});
+            }else{
+              res.data.inviterInfos.forEach(element => {
+                if(ConversionArr.indexOf(element.inviter.toLocaleLowerCase()) != -1){
+                  element.status = true
+                }else{
+                  element.status = false
+                }
+                element.ratio = this.$common.getBit((element.stakedHC / data.toNumber()), 2)
+                element.address = this.$common.getSubStr(element.inviter,5)
+                element.stakedHC = this.$common.getBit(element.stakedHC, 2)
+              })
+              resolve({'arr': res.data.inviterInfos, 'msg':'Success','data':data.toNumber()});
+            }
+          }).catch(() => {
+            reject({'arr': [], 'msg':'Total contract power error'});
+          })
+        }).catch(() => {
+          reject({'arr': [], 'msg':'Database error'});
+        })
       })
     }
   },
@@ -352,11 +488,6 @@ export default {
         color: #fff;
       }
     }
-    .select_address_box{
-      width: 100%;
-      height: 2px;
-      border: 1px solid red;
-    }
     .meinvite{
       width: 100%;
       color: #ffffff;
@@ -384,13 +515,12 @@ export default {
         .positionbox{
           position: absolute;
           left: 50%;
-          top: 62px;
+          top: 30px;
           width: 300px;
           display: flex;
           flex-direction: column;
           align-items: center;
           transform: translateX(-50%);
-          // border: 1px solid red;
           .top_box{
             width: 100%;
             display: flex;
@@ -532,8 +662,6 @@ export default {
         width: 100%;
         display: flex;
         flex-direction: column;
-        // max-height: 540px;
-        // overflow-y: auto;
         .boxs{
           width: 100%;
           display: flex;
@@ -563,6 +691,14 @@ export default {
               padding-left: 10px;
             }
           }
+        }
+        .loadingbox {
+          width: 100%;
+          height: 300px;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          color: #ffffff;
         }
       }
     }
@@ -646,16 +782,107 @@ export default {
           position: absolute;
           left: 50%;
           top: 0.2rem;
+          width: 3rem;
           display: flex;
           flex-direction: column;
           align-items: center;
           transform: translateX(-50%);
-          .span1{
-            color: #DBAE29;
+          .top_box{
+            width: 100%;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            .span1{
+              color: #DBAE29;
+            }
+            .span2{
+              color: #ffffff;
+              font-weight: bold;
+            }
           }
-          .span2{
-            color: #ffffff;
-            font-weight: bold;
+          .bottom_box{
+            width: 100%;
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            margin-top: 0.1rem;
+            padding: 0 0.4rem;
+            .one_line{
+              display: flex;
+              flex-direction: column;
+              align-items: center;
+              .span1{
+                color: #DBAE29;
+              }
+              .span2{
+                color: #ffffff;
+                font-weight: bold;
+              }
+            }
+          }
+        }
+      }
+    }
+    .page_bottom {
+      width: 100%;
+      padding: 0.1rem;
+      background: linear-gradient(90deg, #021F3E 0%, #01142A 100%, #034088 100%);
+      box-shadow: -13px 10px 11px -2px rgba(2, 12, 23, 0.4), -2px -33px 101px 0px rgba(25, 47, 74, 0.5);
+      margin-top: 0.3rem;
+      border-radius: 0.06rem;
+      .embedded_box {
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        .leftbox {
+          display: flex;
+          align-items: center;
+          .img {
+            width: 0.2rem;
+            object-fit: contain;
+          }
+          .span1 {
+            color: #d8d8d8;
+            margin-left: 0.15rem;
+          }
+        }
+        .boxs_reward{
+          width: 100%;
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+          align-items: center;
+          margin-top: 0.2rem;
+          .onebox{
+            width: 100%;
+            height: 0.39rem;
+            background: #032C5B;
+            border-radius: 0.06rem;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            padding: 0 0.2rem;
+            .span1{
+              color: #00F0FF;
+            }
+            .btn_box{
+              display: flex;
+              align-items: center;
+              .btn{
+                width: auto;
+                padding: 0 0.1rem;
+                height: 0.25rem;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-image: url("//cdn.hashland.com/images/extract_btn.png");
+                background-size: 100% 100%;
+                color: #fff;
+                cursor: pointer;
+                margin-left: 0.1rem;
+              }
+            }
           }
         }
       }
@@ -693,13 +920,16 @@ export default {
             text-align: right;
           }
         }
+        .title_onebox{
+          min-width: 0.4rem;
+          color: #ffffff;
+        }
       }
       .bottomline{
         width: 100%;
         display: flex;
         flex-direction: column;
-        max-height: 5rem;
-        overflow-y: auto;
+        margin-top: 0.2rem;
         .boxs{
           width: 100%;
           display: flex;
@@ -717,16 +947,19 @@ export default {
               width: 100%;
               text-align: right;
             }
+          }
+          .title_onebox{
+            min-width: 0.5rem;
+            color: #ffffff;
             .champion_box{
               display: flex;
               .sort1_img{
-                width: 0.17rem;
+                width: 0.2rem;
                 object-fit: contain;
               }
-            }
-            .pad_left{
-              display: flex;
-              padding-left: 0.03rem;
+              .pad_left{
+                padding-left: 0.03rem;
+              }
             }
           }
         }
