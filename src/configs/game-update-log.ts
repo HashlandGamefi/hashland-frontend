87651120abd2562f337fe2b05abb166a9ca08282
Yangx <641160771@qq.com>
2021-12-25 14:36:00 +0800

    Modify GameFi

--- src/configs/game-update-log.ts
+++ src/configs/game-update-log.ts
@@ -1,5 +1,11 @@
 const GameUpdateLog = [
   {
+    version: "1.1.9",
+    update: "25-12-2021",
+    log: "Season 1: Summoner level cap 26, Chapter 1-9, lasts 15 days",
+    downloadLink: "https://cdn.hashland.com/apk/HashWarfare_main_1.1.9.apk",
+  },
+  {
     version: "1.1.7",
     update: "18-12-2021",
     log: "Update Chapter 3-9",
