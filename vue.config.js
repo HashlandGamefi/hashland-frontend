528264d004caa731f5ceff29376f79244760f521
Yangx <641160771@qq.com>
2022-01-19 11:47:11 +0800

    Modify the run packaging configuration

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,24 @@
+const production = process.env.NODE_ENV === "production";
+
 module.exports = {
-  lintOnSave: false,
-  chainWebpack: config => {
-    config.plugins.delete('prefetch'),
+  productionSourceMap: production ? false : true, // 生产环境去掉后缀为.map的文件
+  lintOnSave: true,
+  chainWebpack: (config) => {
+    config.plugins.delete("prefetch"),
       config.module
-      .rule('worker')
-      .test(/\.worker\.js$/)
-      .use('worker-loader').loader('worker-loader')
-      .options({
-        inline: true,
-        fallback: false
-      }).end();
+        .rule("worker")
+        .test(/\.worker\.js$/)
+        .use("worker-loader")
+        .loader("worker-loader")
+        .options({
+          inline: true,
+          fallback: false,
+        })
+        .end();
   },
   configureWebpack: (config) => {
-    if (process.env.NODE_ENV === 'production') {
-      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
-    }
+    if (production)
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;
   },
-  productionSourceMap:false
-}
+  productionSourceMap: false,
+};
